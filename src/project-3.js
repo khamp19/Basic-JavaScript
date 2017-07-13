0a9f3e23ab6a58a71ee7e0eb435dd6bdc12e7723
Kari Schendel <firefaded@yahoo.com>
2017-07-12 19:44:58 -0500

    halfway done

--- src/project-3.js
+++ src/project-3.js
@@ -11,6 +11,8 @@ const addProperty = (object, property) => {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 };
 
 const invokeMethod = (object, method) => {
@@ -22,6 +24,7 @@ const invokeMethod = (object, method) => {
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber * 5;
 };
 
 const deleteProperty = (object, property) => {
@@ -32,6 +35,8 @@ const deleteProperty = (object, property) => {
 const newUser = (name, email, password) => {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  const newObj = { name, email, password };
+  return newObj;
 };
 
 const hasEmail = (user) => {
@@ -43,6 +48,10 @@ const hasProperty = (object, property) => {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  if (object[property]) {
+    return true;
+  }
+  return false;
 };
 
 const verifyPassword = (user, password) => {
@@ -54,6 +63,8 @@ const verifyPassword = (user, password) => {
 const updatePassword = (user, newPassword) => {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  user.password = newPassword;
+  return user;
 };
 
 const addFriend = (user, newFriend) => {
@@ -67,6 +78,10 @@ const setUsersToPremium = (users) => {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  for (let i = 0; i < users.length; i++) {
+    users[i].isPremium = true;
+  }
+  return users;
 };
 
 const sumUserPostLikes = (user) => {
@@ -85,6 +100,10 @@ const addCalculateDiscountPriceMethod = (storeItem) => {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+  storeItem.calculateDiscountPrice = function calculate() {
+    return (storeItem.price - (storeItem.price * storeItem.discountPercentage));
+  };
+  return storeItem;
 };
 
 // Do not modify code below this line.
